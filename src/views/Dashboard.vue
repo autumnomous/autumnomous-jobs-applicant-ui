fca358e1eff1ca237ecf3a380233b301784a8786
Eli Autumn Sanders <easanders@pm.me>
2021-12-18 01:21:31 -0500

    add stub files for some pages

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,54 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    import DefaultLayout from '../layouts/DefaultLayout.vue'
+    import Cookies from 'js-cookie'
+
+    export default {
+        
+        async created(){
+            this.$emit('update:layout', DefaultLayout);
+            this.token = Cookies.get('com.bitjobs')
+
+            this.jobs = await fetch(process.env.VUE_APP_BIT_API_PATH + "/applicant/get/jobs",
+            {
+                  method: "GET",
+                  headers: {
+                      "Content-Type": "application/json",
+                      Authorization: "Bearer " + this.token
+                  }
+              }
+            ).then(result =>{
+
+                if(!result.ok){
+                    console.log(result)
+                    return result
+                }
+                return result.json()
+
+            })
+
+            if(this.jobs){
+                this.dataReady = true
+            }
+
+
+        },
+
+        data(){
+            return { 
+                jobs:[],
+                dataReady: false
+
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
