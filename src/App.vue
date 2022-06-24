d2edb92b433e5ddec888a58416459df6be383d30
Milo Sanders <admin@autumnomous.com>
2022-06-24 00:11:22 -0400

    remove layout event setup, replace with composition api

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,30 @@
 <template>
- <component :is="layout">
-   <router-view v-model:layout='layout'/>
- </component>
-  
-
+  <component :is="header"></component>
+  <router-view/>
+  <component :is="footer"></component>
 </template>
 
 
-<script>
+<script setup>
 
-import Footer from './components/Footer.vue'
-import Header from './components/Header.vue'
-import $ from 'jquery'
+  import Footer from './components/Footer.vue';
+  import Header from './components/Header.vue';
+  import AuthHeader from './components/ui/auth/AuthHeader.vue';
 
-export default {
+  import { useRoute } from 'vue-router';
+  // import { ref, watch } from 'vue'
+  // import $ from 'jquery'
+  
+  const route = useRoute();
+  const routePath = route.path;
+  const header = Header;
+  const footer = Footer;
+  
+  if(routePath.includes("login") || routePath.includes("signup")){
+    header = AuthHeader;
+    footer = null;
+  }
 
-    data(){
-      return {
-        layout:'div',
-      }
-    }
-}
 
 </script>
 <style>
