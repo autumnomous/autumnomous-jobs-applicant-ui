f49ef4a7ea992d3d5953419b139a1065178022a9
Milo Sanders <admin@autumnomous.com>
2022-06-24 00:11:47 -0400

    display bookmarked jobs on applicant Dashboard

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -6,13 +6,10 @@
     <div>
         <h3>Bookmarked Jobs</h3>
 
-        <!-- Card List -->
-        <div class="d-grid gap-5 mb-10">
-            
-            <JobCard v-for="job in jobs" :key="job.publicid" :job="job" ></JobCard>
-            
-        </div>
-        <!-- End Card List -->
+        <Suspense>
+            <JobCardList/>
+        </Suspense>
+        
     </div>
 
     <div>
@@ -24,33 +21,7 @@
 
 
 <script setup>
-    import DefaultLayout from '../layouts/DefaultLayout.vue'
-    import Cookies from 'js-cookie'
-    import JobCard from '../components/listing/JobCard.vue'
-    import {ref} from 'vue'
-
-    const jobs = ref([]);
-    const dataReady = ref(false);
-    const token = Cookies.get('com.ajobs.applicant');
-    const emit = defineEmits(['update:layout']);
-
-    emit('update:layout', DefaultLayout);
-    
-    jobs.value = await fetch(process.env.VUE_APP_BIT_API_PATH + "/applicant/get/bookmark/jobs",{
-                    method:"GET", 
-                    headers:{
-                        "Content-Type": "application/json",
-                        Authorization: "Bearer " + token
-                    }
-                }).then(result => {
-
-                    if(!result.ok){
-                        console.log(result)
-                        return result
-                    }
-                    return result.json()
-                })
-                
+    import JobCardList from '../components/dashboard/JobCardList.vue'
 
     // if(jobs){
     //     dataReady.value = true;
